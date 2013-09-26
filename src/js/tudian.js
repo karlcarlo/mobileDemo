b401de591467cf33be890cfd91de24e57c361c6a
lichunguo <lichunguo@sohu-inc.com>
2013-09-26 18:15:27 +0800

    ad

--- src/js/tudian.js
+++ src/js/tudian.js
@@ -132,10 +132,13 @@
       })
       .on('click', '[data-action="hotspot_goto"]', function(event){
 
-        console.log( '$this.hasClass '+ $(this).hasClass('expanding') );
-
-        event.preventDefault()
-        goto_spot(this)
+        if($(this).hasClass('expanding')){
+            event.preventDefault()
+            back_spot(this)
+        }else{
+          event.preventDefault()
+          goto_spot(this)
+        };
       })
       .on('click', '[data-action="hotspot_back"]', function(event){
         event.preventDefault()
