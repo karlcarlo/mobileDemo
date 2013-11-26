a59fe1e2a6f53fc2cdb5abe7903860deea6b94b5
lichunguo <lichunguo@sohu-inc.com>
2013-11-26 19:00:53 +0800

    add

--- src/js/tudian.js
+++ src/js/tudian.js
@@ -156,7 +156,7 @@
                     var spotID = $(this).attr('data-spot-id');
 
                     var a = $(this).parents().find('.album-wrapper').find('div#spot_'+spotID);
-                    var b = $(a ' [data-action="hotspot_back"]');
+                    var b = $(a ' div[data-action|="hotspot_back"]');
 
                     console.log('b   \n\r  /n/r' + b);
                     console.dir(b.html());
