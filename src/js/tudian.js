f5914f17a8763220e12c513723fca85c008af912
lichunguo <lichunguo@sohu-inc.com>
2013-11-26 18:43:33 +0800

    add

--- src/js/tudian.js
+++ src/js/tudian.js
@@ -155,7 +155,7 @@
 
                     var spotID = $(this).attr('data-spot-id');
 
-                    var a = $(this).parents('.album-wrapper').find('div#spot_'+ spotID);
+                    var a = $(this).parents().find('.album-wrapper').find('div#spot_'+ spotID);
 
                     console.log('a' + a);
                     console.dir(a.html());
