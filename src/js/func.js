68b5ff19c24dc246b8dc494ad0359bc35fd4946a
lichunguo <lichunguo@sohu-inc.com>
2013-12-17 17:38:05 +0800

    ad

--- src/js/func.js
+++ src/js/func.js
@@ -85,7 +85,7 @@
 
           '<div class="photo-wrapper" id="{{dom_id}}">',
           '    <div data-action="hotspot_back"  data-id="{{id}}" data-parent-id="{{parent_id}}" data-type="{{type}}">',
-          '        <img alt="" src="http://s5.suc.itc.cn/ux_tudian/src/asset/mobile/img_download_filler.png" style="background-image: url({{src}}); display: inline-block; width:{{spot_width}}px; height:{{spot_height}}px;" class="photo" draggable="false"/>',
+          '        <img alt="" src="{{src}}" style=" display: inline-block; width:{{spot_width}}px; height:{{spot_height}}px;" class="photo" draggable="false"/>',
           '    </div>',
           '</div>',
 
